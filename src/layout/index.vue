64cfec47a4bf737f243a9c3c0f8f5171158447a3
gaofeng222 <596642721@qq.com>
2024-04-15 18:01:39 +0800

    add:layout页面架子搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,4 +1,12 @@
 <template>
-  <div>模版页面</div>
-  <router-view />
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
 </template>
