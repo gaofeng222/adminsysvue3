154fa9ea2dd779fe5d61c749699c12b5d94378f6
gaofeng222 <596642721@qq.com>
2024-04-16 12:00:53 +0800

    add:完成了用户信息接口联调

--- src/router/persmisson.js
+++ src/router/persmisson.js
@@ -1,7 +1,7 @@
-import router from './index'
-import { useUserStore } from '@/store/modules'
-router.beforeEach((to, from, next) => {
-  const { token } = useUserStore()
+import router from "./index";
+import { useUserStore } from "@/store/modules";
+router.beforeEach(async (to, from, next) => {
+  const { token, getUserInfoDetail, useInfo } = useUserStore();
   // if (!token) {
   //   if (to.path != "/login") {
   //     next("/login");
@@ -15,10 +15,14 @@ router.beforeEach((to, from, next) => {
   //     next();
   //   }
   // }
-  if (!token && to.path != '/login') {
-    return next('/login')
-  } else if (token && to.path == '/login') {
-    return next('/')
+  //如果存在token，则说明已经登录了，如果useInfo为null这时候我们就获取用户信息
+  if (token && !useInfo) {
+    await getUserInfoDetail();
   }
-  next()
-})
+  if (!token && to.path != "/login") {
+    return next("/login");
+  } else if (token && to.path == "/login") {
+    return next("/");
+  }
+  next();
+});
